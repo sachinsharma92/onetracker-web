5edbe22db6f4cc55899c8f18bdc01e88d2d2c7cb
Sachin Sharma <sachin@gryt.fit>
2022-04-15 12:07:49 +0530

    INITIAL: setup

---
+++ pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React, { } from 'react';
+// import "./styles.scss";
+
+export default function HomeScreen() {
+
+  return (
+    <div className='home-section-style'>
+      <h1>Hello</h1>
+    </div>
+  )
+}
